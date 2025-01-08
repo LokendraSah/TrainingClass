80d94f945eb4125635effedde51f232738deb20e
Lokendra Sah <155250768+LokendraSah@users.noreply.github.com>
2025-01-08 13:34:59 +0545

    Arrow function

---
+++ 5_ArrowFunction.js
@@ -0,0 +1,5 @@
+const func1 = () => console.log("this is arrow function")
+func1()
+const addWithArrowFunction = (a,b) => a+b;
+const afResult = addWithArrowFunction (10,18);
+console.log(afResult);
